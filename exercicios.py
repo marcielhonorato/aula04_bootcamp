499f7a36d901a3fe55f4420e3f7d21c8f2c5bafc
marciel_honorato <marciel.hsouza@hotmail.com>
2025-02-27 10:12:50 -0300

    exercicio19 adicionado

--- exercicios.py
+++ exercicios.py
@@ -198,12 +198,32 @@
 # 18 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados 
 # e também indique o menor e o maior valor que estão na tupla
 
-from random import randint
+    # from random import randint
 
-numeros = (randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10))
+    # numeros = (randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10),randint(1, 10))
 
-print(f'Os valores sorteados foram: ', end='')
-for num in numeros:
-    print(f'{num}', end=' ')
-print(f"\nO maior valor sorteado foi {max(numeros)}")
-print(f"O menor valor sorteado foi {min(numeros)}")
+    # print(f'Os valores sorteados foram: ', end='')
+    # for num in numeros:
+    #     print(f'{num}', end=' ')
+    # print(f"\nO maior valor sorteado foi {max(numeros)}")
+    # print(f"O menor valor sorteado foi {min(numeros)}")
+
+# 19 Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final mostre: 
+#A)Quantas vezes apareceu o valor 9 
+#B)Em que posição foi digitado o primeiro valor 3 
+#C)Quais foram os numeros pares
+
+numeros = (int(input("Digite um número: ")),int(input("Digite outro número: ")),int(input("Digite mais um número: ")),int(input("Digite o último um número: ")))
+
+cont_par = 0
+
+print(f"Você digitou os valores: {numeros}")
+print(f"O valor 9 apareceu {numeros.count(9)} vezes")
+if 3 in numeros:
+    print(f"O valor 3 apareceu na {numeros.index(3) + 1}ª posição")
+else: 
+    print(f"O valor 3 não foi digitado")
+print(f"O valores pares digitados foram: ", end='')
+for n in numeros:
+    if n % 2 == 0:
+        print(n, end=' ')
