d6d178128a734ee1cd61a4c943b78bce32a0a31d
marciel_honorato <marciel.hsouza@hotmail.com>
2025-05-30 19:55:43 -0300

    exercicio40 adicionado

--- exercicios.py
+++ exercicios.py
@@ -805,9 +805,49 @@
 #         Olá, Mundo!
 #        ~~~~~~~~~~~~~ 
 
-def escreva(msg):
-    print(f'{"~" * (len(msg) + 4)}')
-    print(f'  {msg}')
-    print(f'{"~" * (len(msg) + 4)}')
+# def escreva(msg):
+#     print(f'{"~" * (len(msg) + 4)}')
+#     print(f'  {msg}')
+#     print(f'{"~" * (len(msg) + 4)}')
 
-escreva('Olá, Mundo!')
+# escreva('Olá, Mundo!')
+
+# 40 Faça um programa que tenha uma função chamada contador(), que receba três parâmetros:inicio,fim e passo e realize a contagem. Seu programa tem que 
+# realizar três contagens através da função criada:
+# A) De 1 até 10, de 1 em 1
+# B) De 10 até 0, de 2 em 2 
+# C) Uma contagem personalizada
+
+from time import sleep
+ 
+def contador(ini:int, fim:int, passo:int):
+    if passo <0:
+        passo *= -1
+    if passo == 0:
+        passo = 1
+    print('-=' * 20)
+    print(f'Contagem de {ini} até {fim} de {passo} em {passo}')
+    sleep(2)
+    if ini < fim:
+        cont = ini
+        while cont <= fim:
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.5)
+            cont += passo
+        print('FIM!')
+    else:
+        cont = ini
+        while cont >= fim:
+            print(f'{cont}', end=' ', flush=True)
+            sleep(0.5)
+            cont -= passo
+        print('FIM!')    
+
+contador(1, 10, 1)
+contador(10, 1, 1)
+print('-=' * 20)
+print("Agora é a sua vez de personaizar a contagem!")
+ini = int(input('Início:    '))
+fim = int(input('Fim:       '))
+passo = int(input('Passo:       '))
+contador(ini, fim, passo)
